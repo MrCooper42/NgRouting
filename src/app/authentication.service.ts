ea820f89ab5a9562315b4cc2902344c969ee22e2
MrCooper42 <mr.cooper42co@gmail.com>
2017-10-12 23:20:10 -0600

    auth service started

---
+++ src/app/authentication.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthenticationService {
+  private loggedIn = false;
+  private user = Object;
+  constructor() {
+    this.loggedIn = true;
+   }
+
+  isLoggedIn() {
+    return this.loggedIn;
+  }
+}
